9fa229bed543296959e87fa8b86079391b327cfa
Schrodawg <cjschroe@ucsc.edu>
2022-10-12 11:20:22 -0700

    lab4 ver 1

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Pseudocode Java computer task.
+ *
+ * Typing in pseudocode, a draft plan of a Minesweeper prototype.
+ Takes user input to generate a game grid and the number of bombs present.
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Craig Schroeder.
+ * @since  10.11.2022
+ */
+
+/** jshint {inline configuration here} */
+
+//Ask for user input to choose board size
+//Ask for another input for number of bombs
+//For every bomb, randomly place one in a grid until all have been placed
+//For every non-bomb tile, detect how many bomb tiles are touching it
+  //Assign non-bomb tiles a number based on how many bombs are nearby
+//Print out the grid visually
