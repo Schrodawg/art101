a39adcbe46ce710522da9db8e6d7fc9fbe36a03f
Schrodawg <cjschroe@ucsc.edu>
2022-10-16 13:07:02 -0700

    Finished Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Craig Schroeder
+ * Created:   10.16.2022
+ *
+ * Lisence: Public Domain
+ **/
+
+//Vars
+var make = "Buick";
+var model = "Regal";
+var color = "Green";
+var year = 2000;
+
+ //Age calc
+var current_year = 2022;
+var age = current_year - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
