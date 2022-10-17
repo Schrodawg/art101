6cf25a13dc304742adefe734390269be5ed9b30b
Schrodawg <cjschroe@ucsc.edu>
2022-10-17 11:54:01 -0700

    Ver 0.1 of Lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Author:    Craig Schroeder
+ * Created:   10.17.2022
+ *
+ * Lisence: Public Domain
+ **/
+
+//Vars
+var myTransport = ["Bus", " Car ", " Legs", " Car of friends"]
+myMainRide = {
+  make : "Buick",
+  model : "Regal",
+  color : "Green",
+  year : 2000,
+  current_year : 2022,
+  age : function() {
+    return current_year - age;
+   }
+}
+
+//output
+myTransport[1];
+document.writeln("Hot Wheels collectors wish they had, <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+document.writeln("<pre>" + "But they're out here taking the " + myTransport + "</pre>");
