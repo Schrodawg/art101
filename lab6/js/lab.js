a2c95588f2e192a5e4670e71ec4ec761374c85ca
Schrodawg <cjschroe@ucsc.edu>
2022-10-18 08:34:02 -0700

    Final ver 1.0 lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -15,11 +15,10 @@ myMainRide = {
   current_year : 2022,
   age : function() {
     return current_year - age;
-   }
+  }
 }
 
 //output
-myTransport[1];
 document.writeln("Hot Wheels collectors wish they had, <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
 document.writeln("<pre>" + "But they're out here taking the " + myTransport + "</pre>");
