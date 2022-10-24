668cd3b5e3e11ff44137733af6f1a51b4a7ac9e6
Schrodawg <cjschroe@ucsc.edu>
2022-10-24 11:21:01 -0700

    Lab 7 final ver 1.0

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -4,20 +4,24 @@
  *
  * Lisence: Public Domain
  **/
+var userName = window.prompt("Please enter your name here for undisclosed reasons");
 
-function sortUserName() {
-  var userName = window.prompt("Please enter your name here for undisclosed reasons");
+function sortUserName(userName) {
   console.log("userName = ", userName);
+  //lowercase
+  var userName = userName.toLowerCase();
   //string -> array
   var nameArray = userName.split('');
   console.log("nameArray = ", nameArray);
   //sort array
   var nameArraySort = nameArray.sort();
   console.log("nameArraySort = ", nameArraySort);
+  //Capitalize first letters
+  nameArraySort[0] = nameArraySort[0].toUpperCase();
   //array -> string
   var nameSorted = nameArraySort.join('');
   console.log("nameSorted = ", nameSorted);
   return nameSorted;
 }
-
-document.writeln("Look what I have created, is it not marvelous? Your name is beautiful now, you're welcome. ", sortUserName(), "</br>");
+//print it out
+document.writeln("Look what I have created, is it not marvelous? Your name is beautiful now, you're welcome. ", sortUserName(userName), "</br>");
