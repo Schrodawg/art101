d694ff4ab21d0f7f6e185dc9f24858ca0712d5c0
Schrodawg <cjschroe@ucsc.edu>
2022-11-16 23:30:10 -0800

    lab 14 fin with lab10 changes

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,30 +4,75 @@
  *
  * Lisence: Public Domain
  **/
+//OLD CODE FROM ORIGINAL SUBMISSION
 
-//Variables
-var outputEl = document.getElementById("output");
-var buttonEl = document.createElement('button');
-var buttonEl2 = document.getElementById("button2");
-var buttonEl3 = document.getElementById("button3");
-var resultEl = document.createElement('p');
+// //Variables
+// var outputEl = document.getElementById("output");
+// var buttonEl = document.createElement('button');
+// var buttonEl2 = document.getElementById("button2");
+// var buttonEl3 = document.getElementById("button3");
+// var resultEl = document.createElement('p');
+//
+// //Button1 id and adding to output
+// buttonEl.id = 'my-button'
+// outputEl.appendChild(buttonEl);
+//
+// //Adding text to all buttons
+// buttonEl.innerHTML = " Button ";
+// buttonEl2.innerHTML = " Button 2 ";
+// buttonEl3.innerHTML = " Button 3 ";
+//
+// //Button1 name and sort function
+// buttonEl.addEventListener("click", function() {
+//   var nameEl = document.getElementById("user-name").value;
+//   //Uses "Test" if nothing is in the box on button1 press
+//   if (nameEl == '') {
+//     var nameEl = "TEST";
+//   }
+//
+//   //Lowerscases the username, splits it into an array, and sorts the array
+//   var sortedNewName = nameEl.toLowerCase().split('').sort();
+//   //Uppercases first letter
+//   sortedNewName[0] = sortedNewName[0].toUpperCase();
+//   //Back to string
+//   sortedNewName = sortedNewName.join('');
+//   //added to a paragraph element
+//   resultEl.innerHTML = sortedNewName;
+//   //console test
+//   console.log(sortedNewName);
+//   //Put into output
+//   outputEl.appendChild(resultEl);
+// })
+//
+// //2nd button element
+// buttonEl2.addEventListener("click", function() {
+//   var bodyEl = document.getElementById('content');
+//   console.log(bodyEl);
+//   //Set background to light blue
+//   bodyEl.style.background = '#95b7ed';
+// })
+//
+// //3rd button element
+// buttonEl3.addEventListener("click", function() {
+//   var bodyEl = document.getElementById("content");
+//   //Set all of text to the best font
+//   bodyEl.style.fontFamily = "Comic Sans MS, cursive";
+// })
 
-//Button1 id and adding to output
-buttonEl.id = 'my-button'
-outputEl.appendChild(buttonEl);
+//DEBUGGING CODE: From Lab14. Created on 11/16/2022
 
-//Adding text to all buttons
-buttonEl.innerHTML = " Button ";
-buttonEl2.innerHTML = " Button 2 ";
-buttonEl3.innerHTML = " Button 3 ";
+//Creating a button and naming each one
+$('#button1').html("Button 1");
+$('#button2').html("Button 2");
+$('#button3').html("Button 3");
 
-//Button1 name and sort function
-buttonEl.addEventListener("click", function() {
-  var nameEl = document.getElementById("user-name").value;
+//Button 1 name sort function
+$("#button1").click(function(){
+  var nameEl = $('#user-name').val();
   //Uses "Test" if nothing is in the box on button1 press
-  if (nameEl == '') {
-    var nameEl = "TEST";
-  }
+   if (nameEl == '') {
+     var nameEl = "TEST";
+   }
 
   //Lowerscases the username, splits it into an array, and sorts the array
   var sortedNewName = nameEl.toLowerCase().split('').sort();
@@ -35,25 +80,19 @@ buttonEl.addEventListener("click", function() {
   sortedNewName[0] = sortedNewName[0].toUpperCase();
   //Back to string
   sortedNewName = sortedNewName.join('');
-  //added to a paragraph element
-  resultEl.innerHTML = sortedNewName;
   //console test
   console.log(sortedNewName);
   //Put into output
-  outputEl.appendChild(resultEl);
+  $('#output').html("<p>" + sortedNewName + "</p>");
+  // })
 })
 
-//2nd button element
-buttonEl2.addEventListener("click", function() {
-  var bodyEl = document.getElementById('content');
-  console.log(bodyEl);
-  //Set background to light blue
-  bodyEl.style.background = '#95b7ed';
+//Button2 color change
+$("#button2").click(function(){
+  $("body").css("background-color", "#95b7ed");
 })
 
-//3rd button element
-buttonEl3.addEventListener("click", function() {
-  var bodyEl = document.getElementById("content");
-  //Set all of text to the best font
-  bodyEl.style.fontFamily = "Comic Sans MS, cursive";
+//Button 3 font change
+$("#button3").click(function(){
+  $("#content").css("font-family", "Comic Sans MS");
 })
